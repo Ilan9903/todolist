a46a012f6d674f1b68927d2b9c7559570c28a794
Ilan ARFI <i.arfipro@outlook.fr>
2025-09-22 14:26:15 +0200

    authentification, routes, login, register, dashboard
    base back & front

--- resources/views/Auth/registration.blade.php
+++ resources/views/Auth/registration.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ToDoList - Login</title>
+    <title>Laravel 12 Custom User Register Page - DevScriptSchool.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <style>
         body{
@@ -17,10 +17,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                <div class="card border border-light-subtle rounded-3 shadow-sm mt-5">
+                <div class="card border border-light-subtle rounded-3 shadow-sm">
                     <div class="card-body p-3 p-md-4 p-xl-5">
-                        <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Sign in to your account</h2>
-                        <form method="POST" action="{{ route('login.post') }}">
+                        <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Sign up to your account</h2>
+                        <form method="POST" action="{{ route('register.post') }}">
                             @csrf
 
                             @session('error')
@@ -32,11 +32,22 @@
                             <div class="row gy-2 overflow-hidden">
                                 <div class="col-12">
                                     <div class="form-floating mb-3">
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="name@example.com" required>
+                                        <label for="name" class="form-label">{{ __('Name') }}</label>
+                                    </div>
+                                    @error('name')
+                                    <span class="text-danger" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                                    @enderror
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-floating mb-3">
                                         <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com" required>
                                         <label for="email" class="form-label">{{ __('Email Address') }}</label>
                                     </div>
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-danger" role="alert">
                           <strong>{{ $message }}</strong>
                       </span>
                                     @enderror
@@ -47,29 +58,29 @@
                                         <label for="password" class="form-label">{{ __('Password') }}</label>
                                     </div>
                                     @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="text-danger" role="alert">
                           <strong>{{ $message }}</strong>
                       </span>
                                     @enderror
                                 </div>
                                 <div class="col-12">
-                                    <div class="d-flex gap-2 justify-content-between">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" name="rememberMe" id="rememberMe">
-                                            <label class="form-check-label text-secondary" for="rememberMe">
-                                                Keep me logged in
-                                            </label>
-                                        </div>
-                                        <a href="#!" class="link-primary text-decoration-none">{{ __('forgot password?') }}</a>
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation" value="" placeholder="password_confirmation" required>
+                                        <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
                                     </div>
+                                    @error('password_confirmation')
+                                    <span class="text-danger" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                                    @enderror
                                 </div>
                                 <div class="col-12">
                                     <div class="d-grid my-3">
-                                        <button class="btn btn-primary btn-lg" type="submit">{{ __('Login') }}</button>
+                                        <button class="btn btn-primary btn-lg" type="submit">{{ __('Register') }}</button>
                                     </div>
                                 </div>
                                 <div class="col-12">
-                                    <p class="m-0 text-secondary text-center">Don't have an account? <a href="{{ route('register') }}" class="link-primary text-decoration-none">Sign up</a></p>
+                                    <p class="m-0 text-secondary text-center">Have an account? <a href="{{ route('login') }}" class="link-primary text-decoration-none">Sign in</a></p>
                                 </div>
                             </div>
                         </form>
